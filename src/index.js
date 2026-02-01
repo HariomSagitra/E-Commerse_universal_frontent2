460e53b4824dec53f607e7563cadbc251f77c594
Hariom Sagitra <hariomdhakad750910@gmail.com>
2026-02-01 17:26:24 +0530

    Remove HashRouter and simplify render method

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
+
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <App />
-// );
-root.render(
-  <HashRouter>
+ root.render(
     <App />
-  </HashRouter>
-);
-
+ );

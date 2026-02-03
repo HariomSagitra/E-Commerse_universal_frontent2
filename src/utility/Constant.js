455ad5466a816a6313be7ee4b56797713a31e969
Hariom Sagitra <hariomdhakad750910@gmail.com>
2026-02-04 01:26:47 +0530

    Update C_URL to use window.location.origin

--- src/utility/Constant.js
+++ src/utility/Constant.js
@@ -55,6 +55,8 @@
 
 //export const C_URL = "http://localhost:3000/"
 //const BASE_URL = "http://localhost:5000/"
+export const C_URL = window.location.origin + "/";
+
 
 //const BASE_URL = process.env.REACT_APP_API_URL + "/";
 const BASE_URL = "https://e-commerse-universal-backend-1.onrender.com/";
